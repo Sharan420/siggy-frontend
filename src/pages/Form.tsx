03302615c0754fc8156b70e72194521bf6ff3b6b
Sharan Suri <sharansuri-8@hotmail.com>
2024-10-19 22:09:09 +0530

    feat: responsive-ness

--- src/pages/Form.tsx
+++ src/pages/Form.tsx
@@ -1,18 +1,15 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { ClipboardIcon, Coins } from "lucide-react";
+import { ClipboardIcon } from "lucide-react";
 
 const Form = () => {
   return (
     <div className="w-screen h-screen flex flex-col items-center bg-background">
-      <div className="h-full w-1/2 flex flex-col justify-center items-center gap-5">
+      <div className="h-full w-[80%] md:w-1/2 flex flex-col justify-center items-center gap-5">
         <div className="flex flex-col justify-center items-center gap-3">
-          <div className="flex flex-row gap-2 items-center">
-            <Coins className="h-[3.25rem] w-auto text-primary" />
-            <h1 className="text-7xl italic font-black text-primary">SIGGY</h1>
-          </div>
+          <h1 className="text-7xl italic font-black text-primary">SIGGY</h1>
 
-          <ol className="list-decimal list-inside text-base font-semibold">
+          <ol className="list-decimal list-inside text-base font-semibold space-y-2">
             <li>
               Open{" "}
               <a
@@ -27,7 +24,7 @@ const Form = () => {
             <li>Paste it below and search</li>
           </ol>
         </div>
-        <div className="flex flex-row w-full max-w-lg gap-5">
+        <div className="flex flex-row w-full max-w-lg gap-2 md:gap-5">
           <Input
             wrapperClassName="w-full"
             placeholder="https://www.swiggy.com/city/hyderabad/social-serlingampally-circle-raidurg-rest800956"
