ac9cfca5a8cc0bba20c15db806da6032c6ee1e05
Sharan Suri <sharansuri-8@hotmail.com>
2024-10-19 21:33:31 +0530

    feat:added form UI

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,80 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  helperText?: string;
+  startAdornment?: React.ReactNode;
+  endAdornment?: React.ReactNode;
+  wrapperClassName?: string;
+  inputClassName?: string;
+  startAdornmentClassName?: string;
+  endAdornmentClassName?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
+  (
+    {
+      className,
+      type,
+      label,
+      helperText,
+      startAdornment,
+      endAdornment,
+      wrapperClassName,
+      inputClassName,
+      startAdornmentClassName,
+      endAdornmentClassName,
+      ...props
+    },
+    ref
+  ) => (
+    <div className={cn("flex flex-col", wrapperClassName)}>
+      {label && <label className="mb-1 text-sm text-gray-700">{label}</label>}
+      <div
         className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "flex items-center h-10 w-full rounded-md border border-input bg-background text-sm ring-offset-background disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+      >
+        {startAdornment && (
+          <span
+            className={cn(
+              "px-3 pr-0 text-sm text-muted-foreground",
+              startAdornmentClassName
+            )}
+          >
+            {startAdornment}
+          </span>
+        )}
+        <input
+          type={type}
+          className={cn(
+            "flex-1 px-3 py-2 placeholder:text-muted-foreground focus:outline-none disabled:opacity-50 rounded-r-sm bg-transparent",
+            inputClassName
+          )}
+          ref={ref}
+          {...props}
+        />
+        {endAdornment && (
+          <span
+            className={cn(
+              "px-3 text-sm text-muted-foreground",
+              endAdornmentClassName
+            )}
+          >
+            {endAdornment}
+          </span>
+        )}
+      </div>
+      {helperText && (
+        <span className="mt-1 text-xs text-muted-foreground">{helperText}</span>
+      )}
+    </div>
+  )
+);
+Input.displayName = "Input";
 
-export { Input }
+export { Input };
